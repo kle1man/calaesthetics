fd26136c95aed79a7b87bd16683df78dcc6e7d36
Alex Kleiman <alexjkleiman@gmail.com>
2024-03-13 17:59:40 -0400

    Added comments

--- app/authentication.py
+++ app/authentication.py
@@ -39,6 +39,7 @@ def createaccount(username, password):
         # writes the username and password inputted as parameters to the file, seperated by a colon
         file.write(f"{username}:{password}\n")
 
+# takes a username as input and deletes the account tied to it from the "credentials.txt" file
 def deleteaccount(username):
     # gets the directory name of this file, and sets a filepath to the "credentials.txt" file in data/
     sourcefile = os.path.join(os.path.dirname(__file__), '../data/credentials.txt')
