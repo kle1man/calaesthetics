78ebe73f67f5a88c521eae9e1ce23dac0a1dfade
Alex Kleiman <alexjkleiman@gmail.com>
2024-03-14 16:47:24 -0400

    Added comments

--- app/menu.py
+++ app/menu.py
@@ -29,30 +29,45 @@ def displaymenu(items, pagenumber):
     startindex = (pagenumber - 1) * 10
     # sets the end index to 10 after the start index to display 10 items per page
     endindex = startindex + 10
+    # finds the total numbers of pages
+    totalpages = (len(items) + 9) // 10
+    # outputs the page number before the items
+    print(f"\nPage {pagenumber}/{totalpages}")
     # iterates through the items array to display menu items
     for i, item in enumerate(items[startindex:endindex], start = startindex + 1):
         # adds increasing numbers before printing the items to make selection easier for the user
         print(f"{i}: {item}")
-    # outputs the page number after the items
-    print(f"Page {pagenumber}")
 
+# uses the displaymenu() function to create a navigatable menu with selectable items 
 def navigatemenu(items):
+    # defaults the page number to 1
     pagenumber = 1
+    # finds the total number of pages needed to display all items inputted
     totalpages = (len(items) + 9) // 10
+    # infinite loop to ensure valid input
     while True:
+        # hooks the displaymenu() function to display all of the items on a specific page
         displaymenu(items, pagenumber)
+        # gathers user input
         action = input("Enter action (n = next page, b = previous page, cancel = cancel menu, exit = exit program): ")
+        # next page
         if action == "n":
             pagenumber = min(pagenumber + 1, totalpages)
+        # previous page
         elif action == "b":
             pagenumber = max(pagenumber - 1, 1)
+        # exit clause
+        elif action == "exit":
+            print("Logged out.")
+            exit()
+        # cancel clause
         elif action == "cancel":
             return None
-        elif action == "exit":
-            exit()
+        # if the input is a digit, returns the menu item corresponding to it
         elif action.isdigit():
             index = int(action) - 1
             if index >= 0 and index <= len(items):
                 return items[index]
+        # if no valid input is provided, loops again
         else:
             print("Invalid input. Try again.")
